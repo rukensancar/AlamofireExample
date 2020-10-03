222a1266afd486182e33045c0f50916b0900abdf
Ruken Sancar <rukensancar@gmail.com>
2020-10-03 16:57:42 +0300

    Initial Commit

---
+++ AlamofireExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireExample
+//
+//  Created by Ruken SANCAR on 3.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
